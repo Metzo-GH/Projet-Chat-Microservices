67dadaf44d2360a2a6d70fd81feca32b9560d36f
Metzo-GH <mouhamedhino@gmail.com>
2025-01-08 23:08:02 +0100

    Add generated source files and Maven metadata for alom services

---
+++ client/target/generated-sources/cxf/alom/services/SendPrivateMessage.java
@@ -0,0 +1,114 @@
+
+package alom.services;
+
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Classe Java pour sendPrivateMessage complex type.</p>
+ * 
+ * <p>Le fragment de schéma suivant indique le contenu attendu figurant dans cette classe.</p>
+ * 
+ * <pre>{@code
+ * <complexType name="sendPrivateMessage">
+ *   <complexContent>
+ *     <restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       <sequence>
+ *         <element name="token" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0" form="qualified"/>
+ *         <element name="recipient" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0" form="qualified"/>
+ *         <element name="message" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0" form="qualified"/>
+ *       </sequence>
+ *     </restriction>
+ *   </complexContent>
+ * </complexType>
+ * }</pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "sendPrivateMessage", propOrder = {
+    "token",
+    "recipient",
+    "message"
+})
+public class SendPrivateMessage {
+
+    protected String token;
+    protected String recipient;
+    protected String message;
+
+    /**
+     * Obtient la valeur de la propriété token.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getToken() {
+        return token;
+    }
+
+    /**
+     * Définit la valeur de la propriété token.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setToken(String value) {
+        this.token = value;
+    }
+
+    /**
+     * Obtient la valeur de la propriété recipient.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getRecipient() {
+        return recipient;
+    }
+
+    /**
+     * Définit la valeur de la propriété recipient.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setRecipient(String value) {
+        this.recipient = value;
+    }
+
+    /**
+     * Obtient la valeur de la propriété message.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getMessage() {
+        return message;
+    }
+
+    /**
+     * Définit la valeur de la propriété message.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setMessage(String value) {
+        this.message = value;
+    }
+
+}
